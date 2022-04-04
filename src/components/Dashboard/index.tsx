316775cfb495b1632283aa33a2000b05b97fa582
Marcio de Carvalho Silva <marciocarvsilva@gmail.com>
2022-04-04 09:49:36 -0300

    first commit

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+
+import { Summary } from "../Summary";
+import { TransctionTable } from "../TransactionTable";
+import { Container } from "./styles";
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Summary />
+            <TransctionTable />
+        </Container>
+    )
+}
